219ba3e76f23bea094005d52df7eb8e10958418a
tracer <tracer@tracer.org>
2018-05-06 11:40:42 +0800

    >  compile ray Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- memhack.c
+++ memhack.c
@@ -223,9 +223,9 @@ void ptrace_write(pid_t pid, void *addr, void *buf, size_t size) {
 
     if (size != 0) {
         long data = ptrace_peekdata(pid, dst);
-        printf("before data: %lx, size: %d", size);
+        printf("before data: %lx, size: %d", data, size);
         memcpy(&data, src, size);
-        printf("after data: %lx, size: %d", size);
+        printf("after data: %lx, size: %d", data, size);
         ptrace_pokedata(pid, dst, data);
     }
 }
