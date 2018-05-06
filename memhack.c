f4d6c117bf4764aba301258594bbf604ba624205
tracer <tracer@tracer.org>
2018-05-06 14:20:20 +0800

    >  compile ray Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- memhack.c
+++ memhack.c
@@ -251,7 +251,7 @@ void init_areas() {
     rc = regcomp(&reg, 
         "[0-9a-fA-F]+\\-[0-9a-fA-f]+\\s+[rwxps-]+\\s+"
         "[0-9a-fA-F]+\\s+[0-9a-fA-F]+:[0-9a-fA-F]+\\s+"
-        "[0-9]+\\s+",
+        "[0-9]+\\s+.*",
         REG_EXTENDED);
     if (rc != 0) {
         regerror(rc, &reg, errbuf, MAXLINE);
